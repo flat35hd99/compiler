e519820ea811316b79ae077bb46bc4778bbbf831
flat35hd99 <flat35hd99@gmail.com>
2023-02-01 00:25:11 +0900

    Step3: Tokenizerを導入

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+// Tokens
+typedef enum {
+    TK_RESERVED, // operators
+    TK_NUM,      // numbers
+    TK_EOF,      // The end of file
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;   // If kind == TK_NUM, then the value
+    char *str; // If kind == TK_RESERVED, then the symbol
+};
+
+// Current token
+Token *token;
+
+// Print error and exit
+// recieve same arguments of printf
+void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// tokenが期待している記号のときはトークンを一つ読み進めてtrue
+// そうでなければfalseを返す
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token->next;
+    return true;
+}
+
+// tokenが期待している記号のときはtokenを一つ読み進める。
+// otherwise report error
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) 
+        error("'%c' expected but it is not", op);
+    
+    token = token->next;
+}
+
+// If token is number, then step to next token and return the current token value.
+// Otherwise report error
+int expect_number() {
+    if (token->kind != TK_NUM)
+        error("It is not number");
+    
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+Token *tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        // Skip whitespaces
+        if (isblank(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            // hoge++はhogeを更新しつつもとの値を返す
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("cannot tokenize: '%c'", *p);
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -7,28 +112,22 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    char *p = argv[1];
+    token = tokenize(argv[1]);
 
+    // アセンブリの前半を出力
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+    printf("  mov rax, %d\n", expect_number());
 
-    while (*p) {
-        if (*p == '+') {
-            p++; // ポインタを32bitずらす。1語分か?
-            printf("  add rax, %ld\n", strtol(p, &p, 10));
+    while(!at_eof()) {
+        if (consume('+')) {
+            printf("  add rax, %d\n", expect_number());
             continue;
         }
 
-        if (*p == '-') {
-            p++;
-            printf("  sub rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        }
-
-        fprintf(stderr, "unexpected value: '%c'\n", *p);
-        return 1;
+        expect('-');
+        printf("  sub rax, %d\n", expect_number());
     }
 
     printf("  ret\n");
--- test.sh
+++ test.sh
@@ -5,7 +5,16 @@ assert() {
   input="$2"
 
   ./9cc "$input" > tmp.s
+  if [ "$?" != "0" ];then
+    echo "9cc process finished in fail"
+    exit 1;
+  fi
   cc -o tmp tmp.s
+  if [ "$?" != "0" ];then
+    echo "cc process finished in fail"
+    exit 1;
+  fi
+
   ./tmp
   actual=$?
 
@@ -17,12 +26,16 @@ assert() {
   fi
 }
 
-# Step1
+# Step1: 整数
 assert 0 0
 assert 42 42
 
-# Step2
+# Step2: 加減算
 assert 21 '5+20-4'
 assert 42 '100-50-8'
+
+# Step3: Tokenizer
+assert 2 '5 - 3'
+assert 42 ' 51   - 12+3 '
 
 echo OK
