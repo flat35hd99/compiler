cc3c5521900d157e35f6963839f3715501b4f9d8
flat35hd99 <flat35hd99@gmail.com>
2023-04-22 19:11:50 +0900

    Step5: 再帰降下

--- test.sh
+++ test.sh
@@ -38,4 +38,11 @@ assert 42 '100-50-8'
 assert 2 '5 - 3'
 assert 42 ' 51   - 12+3 '
 
+# Step4: LL(1)
+assert 4 '10*4 - 4*9'
+assert 2 '(4+2)/(3)'
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
+
 echo OK
